47384e94753fabb65bc9e6085920ea84feee0ae5
Salih ACUR <salihacur1@gmail.com>
2020-12-12 16:31:19 +0300

    ajax succeded on temp tsp file

--- project-flask/app.py
+++ project-flask/app.py
@@ -1,3 +1,4 @@
+import time
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -5,9 +6,11 @@ import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 
-import os 
+import os
 from io import BytesIO
-import math, random, timeit
+import math
+import random
+import timeit
 
 from helpers import helper
 from algorithms.tsp.aco_tsp_solve import ACO_TSP_SOLVE
@@ -16,41 +19,50 @@ from algorithms.tsp.ga_tsp_solve import GA_TSP_SOLVE
 from flask import Flask, render_template, request, make_response, redirect, abort, flash, url_for, jsonify
 from werkzeug.utils import secure_filename
 
-#import warnings 
-#warnings.filterwarnings("ignore")
+# import warnings
+# warnings.filterwarnings("ignore")
 
 YUKLEME_KLASORU = 'static/dataUpload'
-#Test amaçlı duruyorlar, son hali => .csv ve .txt olcak
-UZANTILAR = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif','csv'])
+# Test amaçlı duruyorlar, son hali => .csv ve .txt olcak
+UZANTILAR = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'csv'])
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '^%huYtFd90;90jjj'
+app.config['UPLOADED_DATAS'] = 'uploads'
+
 app.config['UPLOAD_FOLDER'] = YUKLEME_KLASORU
 app.secret_key = "denemeKey"
 
 
 def uzanti_kontrol(dosyaadi):
     return '.' in dosyaadi and \
-    dosyaadi.rsplit('.', 1)[1].lower() in UZANTILAR
+        dosyaadi.rsplit('.', 1)[1].lower() in UZANTILAR
+
 
 helper.run_schedule()
+
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
+
 @app.route('/optimization')
 def optimization():
     return render_template("optimization.html")
+
 
 @app.route('/machine_learning')
 def machine_learning():
     return render_template("machine-learning.html")
 
+
 @app.route('/travellingsalesmanproblem')
 def tsp():
     return render_template("tsp.html")
 
-import time
+
 @app.route('/process2', methods=['POST'])
 def process():
 
@@ -60,58 +72,60 @@ def process():
     if name and email:
         datas = email + name
         time.sleep(5)
-        return jsonify({'name' : datas})
+        return jsonify({'name': datas})
 
-    return jsonify({'error' : 'Missing data!'})
+    return jsonify({'error': 'Missing data!'})
+
 
 @app.route('/tsptest', methods=['POST'])
 def tsptest():
     file_type = "csv"
     if file_type == "text":
-        df = pd.read_text("../input/marmara_mesafe/{}".format("mesafeler.txt")) 
+        df = pd.read_text("../input/marmara_mesafe/{}".format("mesafeler.txt"))
     if file_type == "csv":
         df = pd.read_csv("data/marmara_mesafeler.csv")
 
     cities = df.loc[:, 'city'].values
 
-    cities_x_axis = df.loc[:,'longitude'].values  # longitude is x axis
+    cities_x_axis = df.loc[:, 'longitude'].values  # longitude is x axis
 
-    cities_y_axis = df.loc[:,'latitude'].values # latitude is y axis
+    cities_y_axis = df.loc[:, 'latitude'].values  # latitude is y axis
 
     antsize = request.form['antsize']
     print(antsize)
     pheromonerho = request.form['pheromonerho']
-    print("rho",pheromonerho)
+    print("rho", pheromonerho)
     alpha = request.form['alfa']
-    print("alfa",alpha)
+    print("alfa", alpha)
     beta = request.form['beta']
     iteration = request.form['iteration']
-    
+
     print(alpha + " " + beta)
 
     if antsize and pheromonerho and alpha and beta:
         start = timeit.default_timer()
-        
-        cities_xy = helper.format_for_genetic(cities_x_axis,cities_y_axis)
-        
-        aco_tsp = ACO_TSP_SOLVE(_x_axis = cities_x_axis,
-                                _y_axis = cities_y_axis,
-                                _iteration = int(iteration),
-                                _ant_size = int(antsize),
-                                _rho = float(pheromonerho),
-                                _alpha = float(alpha),
-                                _beta = float(beta),
-                                _cities = cities)
+
+        cities_xy = helper.format_for_genetic(cities_x_axis, cities_y_axis)
+
+        aco_tsp = ACO_TSP_SOLVE(_x_axis=cities_x_axis,
+                                _y_axis=cities_y_axis,
+                                _iteration=int(iteration),
+                                _ant_size=int(antsize),
+                                _rho=float(pheromonerho),
+                                _alpha=float(alpha),
+                                _beta=float(beta),
+                                _cities=cities)
 
         aco_best_route, aco_cost_values, aco_best_cost = aco_tsp.run_optimize()
 
-        ga_tsp = GA_TSP_SOLVE(_cities_xy = cities_xy,
-                        _cities = cities,
-                        _life_count = 100,
-                        _cross_rate = 0.4,
-                        _mutation_rate = 0.2)
+        ga_tsp = GA_TSP_SOLVE(_cities_xy=cities_xy,
+                              _cities=cities,
+                              _life_count=100,
+                              _cross_rate=0.4,
+                              _mutation_rate=0.2)
 
-        ga_best_route, ga_cost_values, ga_best_cost  = ga_tsp.run(int(iteration))
+        ga_best_route, ga_cost_values, ga_best_cost = ga_tsp.run(
+            int(iteration))
 
         # bitti
         stop = timeit.default_timer()
@@ -119,21 +133,27 @@ def tsptest():
         show_label = True
 
         # save routes figure
-        plt_compare_routes_fig = helper.compare_route_graphic(cities_x_axis, cities_y_axis, cities, aco_best_route, ga_best_route)
-        compare_route_fig_path = helper.save_figures_to_upload(plot_fig = plt_compare_routes_fig, img_name = "plt_compare_routes.png")
+        plt_compare_routes_fig = helper.compare_route_graphic(
+            cities_x_axis, cities_y_axis, cities, aco_best_route, ga_best_route)
+        compare_route_fig_path = helper.save_figures_to_upload(
+            plot_fig=plt_compare_routes_fig, img_name="plt_compare_routes.png")
 
         # save cost figure
-        plt_compare_costs_fig = helper.compare_cost_values(aco_cost_values, ga_cost_values)
-        compare_cost_fig_path = helper.save_figures_to_upload(plot_fig = plt_compare_costs_fig, img_name = "plt_compare_costs.png")
-        
-        #compare_route_fig_path = "static/uploads/karınca.png"
-        print("onay {} resim yolu : {}".format(show_label, compare_route_fig_path))
+        plt_compare_costs_fig = helper.compare_cost_values(
+            aco_cost_values, ga_cost_values)
+        compare_cost_fig_path = helper.save_figures_to_upload(
+            plot_fig=plt_compare_costs_fig, img_name="plt_compare_costs.png")
+
+        # compare_route_fig_path = "static/uploads/karınca.png"
+        print("onay {} resim yolu : {}".format(
+            show_label, compare_route_fig_path))
 
         time.sleep(5)
 
-        return jsonify({'img1' : compare_route_fig_path, 'img2': compare_cost_fig_path})
+        return jsonify({'img1': compare_route_fig_path, 'img2': compare_cost_fig_path})
 
-    return jsonify({'error' : 'Missing data!'})
+    return jsonify({'error': 'Missing data!'})
+
 
 @app.route('/salih')
 def salih():
@@ -149,45 +169,54 @@ def salih():
         Message = {"Message":"Python Says Hello"}
     return render_template("salih.html")
 """
+
+
 @app.route('/berkay')
 def berkay():
 
     return render_template("berkay.html")
 
-#Formdan gelen resmi kullanıcıya geri göster. Veya belgesyi
+# Formdan gelen resmi kullanıcıya geri göster. Veya belgesyi
+
+
 @app.route('/berkay/<string:dosya>')
 def dosyayuklemesonuc(dosya):
-   return render_template("berkay.html", dosya=dosya)
+    return render_template("berkay.html", dosya=dosya)
 
 # Form ile dosya yüklemek
+
+
 @app.route('/dataUpload', methods=['POST'])
 def dosyayukle():
     if request.method == 'POST':
-        #Formdan bize bir dosya geldi mi ?
+        # Formdan bize bir dosya geldi mi ?
         if 'dosya' not in request.files:
             flash('Dosya seçilmedi')
-            return redirect('berkay')         
-							
-		#Kullanıcı dosya seçmemiş olabilir veya tarayıcı boş göndermiş mi kontrol et.
-        dosya = request.files['dosya']					
+            return redirect('berkay')
+
+            # Kullanıcı dosya seçmemiş olabilir veya tarayıcı boş göndermiş mi kontrol et.
+        dosya = request.files['dosya']
         if dosya.filename == '':
             flash('Dosya seçilmedi')
             return redirect('berkay')
-					
-		#Gelen dosya tipi bizim istediğim tipte bir dosya mı ? 
-        #secure_filename => adı "../../../../home/images/logo.png" ise "home_images_logo.png" şeklinde çevirir. 
+
+            # Gelen dosya tipi bizim istediğim tipte bir dosya mı ?
+        # secure_filename => adı "../../../../home/images/logo.png" ise "home_images_logo.png" şeklinde çevirir.
         if dosya and uzanti_kontrol(dosya.filename):
             dosyaadi = secure_filename(dosya.filename)
             dosya.save(os.path.join(app.config['UPLOAD_FOLDER'], dosyaadi))
-            #return redirect(url_for('berkay',dosya=dosyaadi))
+            # return redirect(url_for('berkay',dosya=dosyaadi))
             return redirect('berkay/' + dosyaadi)
         else:
-            flash('İzin verilmeyen dosya uzantısı. Lütfen .txt veya .csv uzantılı bir dosya yükleyiniz !')
-            return redirect('berkay')							
+            flash(
+                'İzin verilmeyen dosya uzantısı. Lütfen .txt veya .csv uzantılı bir dosya yükleyiniz !')
+            return redirect('berkay')
     else:
-        abort(401)      
+        abort(401)
 
-#Cache Blocker
+# Cache Blocker
+
+
 @app.after_request
 def add_header(r):
     """
@@ -200,84 +229,102 @@ def add_header(r):
     r.headers['Cache-Control'] = 'public, max-age=0'
     return r
 
+
 @app.route('/tsptest2', methods=['POST'])
 def tsptest2():
+    file = request.files['cityfile']
+    filename = helper.combine_names()
+    filename += secure_filename(file.filename)
 
-    file_type = "csv"
-    if file_type == "text":
-        df = pd.read_text("../input/marmara_mesafe/{}".format("mesafeler.txt")) 
-    if file_type == "csv":
-        df = pd.read_csv("data/marmara_mesafeler.csv")
+    file.save(os.path.join(app.config['UPLOADED_DATAS'], filename))
+
+    # file name
+    print(file.filename, type(file), file.filename.split('.')[0])
+
+    # file extension -1 yapıyor
+    print(file.filename, type(file), file.filename.split('.')[-1])
+
+    file_extension = str(file.filename.split('.')[-1])
+
+    if file_extension == "txt":
+        df = pd.read_text("uploads/{}".format(filename))
+    if file_extension == "csv":
+        df = pd.read_csv("uploads/{}".format(filename))
 
     cities = df.loc[:, 'city'].values
 
-    cities_x_axis = df.loc[:,'longitude'].values  # longitude is x axis
+    cities_x_axis = df.loc[:, 'longitude'].values  # longitude is x axis
 
-    cities_y_axis = df.loc[:,'latitude'].values # latitude is y axis
+    cities_y_axis = df.loc[:, 'latitude'].values  # latitude is y axis
 
     show_label = False
 
-
     start = timeit.default_timer()
-    
-    ant_size = request.form['antsize'] # id veya name ismi 
-    print(ant_size) #eğer form on submit ile yapılıyorsa data kısmında verdiğimiz isim buraya taşınır
-    pheromone_rho = request.form['rho']
-    print("rho",pheromone_rho)
-    alpha = request.form['alpha']
-    print("alfa",alpha)
-    beta = request.form['beta']
+
+    ant_size = request.form.get('ant_size', False)
+
+    pheromone_rho = request.form.get('pheromone_rho', False)
+
+    alpha = request.form.get('alpha', False)
+
+    beta = request.form.get('beta', False)
 
     # genetik parametreleri
-    life_count = request.form['lifecount']
-    mutation_rate = request.form['mutationrate']
-    cross_rate = request.form['crossrate']
+    life_count = request.form.get('life_count', False)
+    mutation_rate = request.form.get('mutation_rate', False)
+    cross_rate = request.form.get('cross_rate', False)
 
     # iterasyon
-    iteration = request.form['iteration']
+    iteration = request.form.get('iteration', False)
 
     if ant_size and iteration:
-        cities_xy = helper.format_for_genetic(cities_x_axis,cities_y_axis)
-    
-        aco_tsp = ACO_TSP_SOLVE(_x_axis = cities_x_axis,
-                                _y_axis = cities_y_axis,
-                                _iteration = int(iteration),
-                                _ant_size = int(ant_size),
-                                _rho = float(pheromone_rho),
-                                _alpha = float(alpha),
-                                _beta = float(beta),
-                                _cities = cities)
+        cities_xy = helper.format_for_genetic(cities_x_axis, cities_y_axis)
+
+        aco_tsp = ACO_TSP_SOLVE(_x_axis=cities_x_axis,
+                                _y_axis=cities_y_axis,
+                                _iteration=int(iteration),
+                                _ant_size=int(ant_size),
+                                _rho=float(pheromone_rho),
+                                _alpha=float(alpha),
+                                _beta=float(beta),
+                                _cities=cities)
 
         aco_best_route, aco_cost_values, aco_best_cost = aco_tsp.run_optimize()
 
-        ga_tsp = GA_TSP_SOLVE(_cities_xy = cities_xy,
-                                _cities = cities,
-                                _life_count = int(life_count),
-                                _cross_rate = float(cross_rate),
-                                _mutation_rate = float(mutation_rate))
+        ga_tsp = GA_TSP_SOLVE(_cities_xy=cities_xy,
+                              _cities=cities,
+                              _life_count=int(life_count),
+                              _cross_rate=float(cross_rate),
+                              _mutation_rate=float(mutation_rate))
 
-        ga_best_route, ga_cost_values, ga_best_cost  = ga_tsp.run(int(iteration))
+        ga_best_route, ga_cost_values, ga_best_cost = ga_tsp.run(
+            int(iteration))
 
         # bitti
         stop = timeit.default_timer()
-        print(start , "" , stop , "" , ga_best_cost , " " ,aco_best_cost)
+        print(start, "", stop, "", ga_best_cost, " ", aco_best_cost)
 
         show_label = True
 
         # save routes figure
-        plt_compare_routes_fig = helper.compare_route_graphic(cities_x_axis, cities_y_axis, cities, aco_best_route, ga_best_route)
-        compare_route_fig_path = helper.save_figures_to_upload(plot_fig = plt_compare_routes_fig, img_name = "plt_compare_routes.png")
+        plt_compare_routes_fig = helper.compare_route_graphic(
+            cities_x_axis, cities_y_axis, cities, aco_best_route, ga_best_route)
+        compare_route_fig_path = helper.save_figures_to_upload(
+            plot_fig=plt_compare_routes_fig, img_name="plt_compare_routes.png")
 
         # save cost figure
-        plt_compare_costs_fig = helper.compare_cost_values(aco_cost_values, ga_cost_values)
-        compare_cost_fig_path = helper.save_figures_to_upload(plot_fig = plt_compare_costs_fig, img_name = "plt_compare_costs.png")
-        
-        #compare_route_fig_path = "static/uploads/karınca.png"
-        print("onay {} resim yolu : {}".format(show_label, compare_route_fig_path))
+        plt_compare_costs_fig = helper.compare_cost_values(
+            aco_cost_values, ga_cost_values)
+        compare_cost_fig_path = helper.save_figures_to_upload(
+            plot_fig=plt_compare_costs_fig, img_name="plt_compare_costs.png")
 
-        return jsonify({'img1' : compare_route_fig_path, 'img2': compare_cost_fig_path})
+        # compare_route_fig_path = "static/uploads/karınca.png"
+        print("onay {} resim yolu : {}".format(
+            show_label, compare_route_fig_path))
 
-    return jsonify({'error' : 'Missing data!'})
+        return jsonify({'img1': compare_route_fig_path, 'img2': compare_cost_fig_path})
+
+    return jsonify({'error': 'Missing data!'})
 
 
 """
@@ -348,7 +395,7 @@ def run2():
         plt_compare_costs_fig = helper.compare_cost_values(aco_cost_values, ga_cost_values)
         compare_cost_fig_path = helper.save_figures_to_upload(plot_fig = plt_compare_costs_fig, img_name = "plt_compare_costs.png")
         
-        #compare_route_fig_path = "static/uploads/karınca.png"
+        # compare_route_fig_path = "static/uploads/karınca.png"
         print("onay {} resim yolu : {}".format(show_label, compare_route_fig_path))
 
         return render_template("tsp.html", route_compare_img = compare_route_fig_path, 
@@ -358,5 +405,5 @@ def run2():
     else:
         return render_template("tsp.html", onayli = False)
 """
-if __name__ =="__main__":  
-    app.run(debug = True)  
+if __name__ == "__main__":
+    app.run(debug=True)
--- project-flask/helpers/helper.py
+++ project-flask/helpers/helper.py
@@ -1,14 +1,16 @@
-import os, glob
+import os
+import glob
 from io import BytesIO
+from datetime import datetime
 
-import random, string
+import random
+import string
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 from apscheduler.schedulers.background import BackgroundScheduler
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-
 
 from flask import make_response
 
@@ -17,66 +19,74 @@ plt.style.use('ggplot')
 global get_XY_location
 def get_XY_location(x_axis, y_axis, index):
     loc_xy = np.array([x_axis[index], y_axis[index]])
-    return loc_xy 
+    return loc_xy
 
 def compare_cost_values(aco_cost_values, ga_cost_values):
     fig, ax = plt.subplots(1)
     fig.suptitle('ACO vs GA Costs per Iterations')
     plt.xlabel("Iteration")
     plt.ylabel("Cost")
-    plt.grid(b = True, which = 'major', ls = '-.', lw = 0.45)
-    plt.plot(aco_cost_values, c = "orange", label='Karınca')
-    plt.plot(ga_cost_values,'-.',c = "#337ab7",label='Genetik')
+    plt.grid(b=True, which='major', ls='-.', lw=0.45)
+    plt.plot(aco_cost_values, c="orange", label='Karınca')
+    plt.plot(ga_cost_values, '-.', c="#337ab7", label='Genetik')
     plt.legend(['Ant Colony', 'Genetic'])
-    #plt.show()
+    # plt.show()
 
     return fig
 
-def compare_route_graphic( x_axis, y_axis, cities, aco_best_route, ga_best_route):
-    
-    fig, ax = plt.subplots(1, figsize=(12,8))
+
+def compare_route_graphic(x_axis, y_axis, cities, aco_best_route, ga_best_route):
+
+    fig, ax = plt.subplots(1, figsize=(12, 8))
 
     fig.suptitle('ACO vs GA Optimization for TSP Problem')
     plt.xlabel('X AXIS')
     plt.ylabel('Y AXIS')
 
     # city points
-    ax.scatter(x_axis, y_axis, c = "orange", s = 150)
-    # and city names 
+    ax.scatter(x_axis, y_axis, c="orange", s=150)
+    # and city names
     for i in range(len(cities)):
-        ax.annotate(cities[i] , xy = get_XY_location(x_axis, y_axis, i), c = "black")
+        ax.annotate(cities[i], xy=get_XY_location(
+            x_axis, y_axis, i), c="black")
 
     # aco path
     aco_path = np.append(aco_best_route, aco_best_route)
 
     # ga path
     ga_path = np.append(ga_best_route, ga_best_route)
-    
+
     # adding paths to plot
     # aco path plot
-    plt.plot(x_axis[aco_path], y_axis[aco_path], c = "purple", label='Karınca')
+    plt.plot(x_axis[aco_path], y_axis[aco_path], c="purple", label='Karınca')
     # ga path plot
-    plt.plot(x_axis[ga_path], y_axis[ga_path],"-_", c = "green", label='Genetik')
+    plt.plot(x_axis[ga_path], y_axis[ga_path],
+             "-_", c="green", label='Genetik')
 
     plt.legend(['Ant Colony', 'Genetic'])
 
-    #plt.show()
+    # plt.show()
 
     return fig
 
-#Random str generator for image names
+# Random str generator for image names
+
+
 def get_random_string(length):
     letters = string.ascii_letters
     value = random.randint(100, 999)
     result_str = ''.join(random.choice(letters) for i in range(length))
     result_str += str(value)
+    result_str += "_"
     print("Random string of length", length, "is:", result_str)
     return result_str
 
-from datetime import datetime
 
 def get_current_time():
-    return str(datetime.utcnow().strftime('%Y_%m_%d%H_%M_%S_%f')[:-3])
+    return str(datetime.utcnow().strftime('%Y_%m_%d_%H_%M_%S_%f_')[:-3])
+
+def combine_names():
+    return str(get_current_time() + get_random_string(10))
 
 def save_figures_to_upload(plot_fig, img_name):
     # random + img_name
@@ -92,14 +102,16 @@ def save_figures_to_upload(plot_fig, img_name):
 
     plt.savefig(img_path)
     print("Image Path => ", img_path)
-    
+
     return img_path
 
 # using >  plt_img1_path = save_figures_to_upload(figure,your_img_name)
 
-def format_for_genetic(longitudes_x,latitudes_y):
-    distance_list = zip(longitudes_x,latitudes_y)
+
+def format_for_genetic(longitudes_x, latitudes_y):
+    distance_list = zip(longitudes_x, latitudes_y)
     return list(distance_list)
+
 
 def delete_files_in_folder():
     dir = 'static/uploads'
@@ -108,12 +120,14 @@ def delete_files_in_folder():
         for f in filelist:
             os.remove(f)
 
-def delete_files_in_folder_by_dir(dir = ''):
+
+def delete_files_in_folder_by_dir(dir=''):
     temp_dir = dir
     filelist = glob.glob(os.path.join(temp_dir, "*"))
     if len(filelist) != 0:
         for f in filelist:
             os.remove(f)
+
 
 def delete_files_in_folder_by_file_length():
     dir = 'static/uploads'
@@ -121,6 +135,7 @@ def delete_files_in_folder_by_file_length():
     if len(filelist) >= 10:
         for f in filelist:
             os.remove(f)
+
 
 def run_schedule2():
     sched = BackgroundScheduler(daemon=True)
@@ -130,5 +145,5 @@ def run_schedule2():
 
 def run_schedule():
     sched = BackgroundScheduler(daemon=True)
-    sched.add_job(delete_files_in_folder_by_file_length, 'interval', minutes = 1)
+    sched.add_job(delete_files_in_folder_by_file_length, 'interval', minutes=1)
     sched.start()
